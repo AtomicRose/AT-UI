0f0beb7cfabb62c9049552089a731779ff0f77f5
AtomicRose <baishenfang@hotmail.com>
2018-10-18 09:35:51 +0800

    add border-radius add btn hover styles

--- pages/style/style.js
+++ pages/style/style.js
@@ -40,6 +40,32 @@ export default class Style extends React.Component {
                             <button type="button" className="at-btn theme-purple purely">purple</button>
                         </div>
                     </div>
+                    <div className="col-24" style={{ marginTop: '16px' }}>
+                        <div className="at-btn-group">
+                            <button type="button" className="at-btn border-radius">default</button>
+                            <button type="button" className="at-btn theme-light-primary border-radius">light-primary</button>
+                            <button type="button" className="at-btn theme-primary border-radius">primary</button>
+                            <button type="button" className="at-btn theme-dark-primary border-radius">dark-primary</button>
+                            <button type="button" className="at-btn theme-info border-radius">info</button>
+                            <button type="button" className="at-btn theme-success border-radius">success</button>
+                            <button type="button" className="at-btn theme-warning border-radius">warning</button>
+                            <button type="button" className="at-btn theme-danger border-radius">danger</button>
+                            <button type="button" className="at-btn theme-purple border-radius">purple</button>
+                        </div>
+                    </div>
+                    <div className="col-24" style={{ marginTop: '16px' }}>
+                        <div className="at-btn-group">
+                            <button type="button" className="at-btn purely border-radius">default</button>
+                            <button type="button" className="at-btn theme-light-primary purely border-radius">light-primary</button>
+                            <button type="button" className="at-btn theme-primary purely border-radius">primary</button>
+                            <button type="button" className="at-btn theme-dark-primary purely border-radius">dark-primary</button>
+                            <button type="button" className="at-btn theme-info purely border-radius">info</button>
+                            <button type="button" className="at-btn theme-success purely border-radius">success</button>
+                            <button type="button" className="at-btn theme-warning purely border-radius">warning</button>
+                            <button type="button" className="at-btn theme-danger purely border-radius">danger</button>
+                            <button type="button" className="at-btn theme-purple purely border-radius">purple</button>
+                        </div>
+                    </div>
                 </div>
             </div>  
         )
